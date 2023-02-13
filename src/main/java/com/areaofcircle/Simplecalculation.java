7682cfeb8fe0d55b8d8c0329dea0d74d2d20fd5c
Mishal <121633206+RDX00@users.noreply.github.com>
2023-02-13 00:41:36 -0800

    Add files via upload

---
+++ src/main/java/com/areaofcircle/Simplecalculation.java
@@ -0,0 +1,20 @@
+
+package com.simple;
+
+public class Simplecalculation {
+    public static void main(String[] args) {
+        int a,b,result;
+        a=10;
+        b=3;
+        
+        result = a+b;
+        System.out.println(result);
+        result = a-b;
+        System.out.println(result);
+        result = a*b;
+        System.out.println(result);
+        result = a/b;
+        System.out.println(result);
+        
+    }
+}

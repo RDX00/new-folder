9b2bcba9ef5d9ed46b6ca48710107c0e65497143
Mishal <121633206+RDX00@users.noreply.github.com>
2023-02-13 00:41:03 -0800

    Add files via upload

--- src/main/java/com/areaofcircle/Myname.java
+++ src/main/java/com/areaofcircle/Myname.java
@@ -1,13 +1,9 @@
 
-package com.myseslf;
+package com.name;
 
 public class Myname {
-    public static void main(String[] args) {
-        
-        System.out.println("Atakharul Haque Mishal");
-        System.out.println("ID:221-15-4992");
-        System.out.println("Dept:BSc in CSE");
-        System.out.println("Address:Brahmanbaria,Bangladesh");
-        System.out.println("Favourite Game:PUBGs");
+     public static void main(String[] args) {
+        String Name =("Atakharul Haque Mishal");
+        System.out.println(Name);
     }
 }
